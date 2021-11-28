371a2c079d00510b1826fc1aa3cc66cc93c0c0eb
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-28 19:36:53 +0800

    Delete db directory

--- db/migrate/20140814135902_create_donation.rb
+++
@@ -1,9 +0,0 @@
-class CreateDonation < ActiveRecord::Migration
-  def change
-    create_table :donations do |t|
-      t.string :email
-      t.integer :amount_cents
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20140814140259_add_project_to_donation.rb
+++
@@ -1,5 +0,0 @@
-class AddProjectToDonation < ActiveRecord::Migration
-  def change
-    add_column :donations, :project, :string
-  end
-end
